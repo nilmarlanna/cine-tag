8da80c80b472137e7dc8df0f999dc879ba68ee91
nilmarlanna <nilmar.lanna@outlook.com.br>
2023-05-05 17:21:24 -0300

    ultimo commit

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,11 +1,31 @@
-import Cabecalho from "components/Cabecalho";
-import Rodape from "components/Rodape";
+import Banner from "components/Banner";
+import Card from "components/Card";
+import Titulo from "components/Titulo";
+import { useEffect, useState } from "react";
+import styles from './Inicio.module.css';
 
 function Inicio() {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/monicahillman/cinetag-api/videos')
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideos(dados)
+            })
+    }, [])
+
     return (
         <>
-            <Cabecalho />
-            <Rodape />
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
         </>
     )
 }
